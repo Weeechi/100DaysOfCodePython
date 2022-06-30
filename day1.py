9ac18f71cad5d79ba83f8103e0952d0df660ddf3
Weechi <Weechi@MacBook-Pro.local>
2022-06-30 16:28:11 -0400

    day1

---
+++ day1.py
@@ -0,0 +1,43 @@
+# Question 1 
+#Write your code below this line 👇
+
+print('Day 1 - Python Print Function')
+print('The function is declared like this:')
+print("print('what to print')")
+
+
+# Question 2
+
+#Fix the code below 👇
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the "'"+"'" sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+
+#Question 3
+#Write your code below this line 👇
+print(len( input('What is your name?')))
+
+# Question 4 
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+cup1 = 0
+
+cup1 = a
+a = b 
+b = cup1
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
+
