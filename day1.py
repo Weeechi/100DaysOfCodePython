33282fdf1da5ec26cfdd9aff849b84be4bc50fb2
Weechi <Weechi@MacBook-Pro.local>
2022-07-13 13:05:22 -0400

    day2

--- day1.py
+++ day1.py
@@ -1,43 +1,79 @@
-# Question 1 
-#Write your code below this line 👇
+# # Question 1 
+# #Write your code below this line 
 
-print('Day 1 - Python Print Function')
-print('The function is declared like this:')
-print("print('what to print')")
-
-
-# Question 2
-
-#Fix the code below 👇
-
-print("Day 1 - String Manipulation")
-print("String Concatenation is done with the "'"+"'" sign.")
-print('e.g. print("Hello " + "world")')
-print("New lines can be created with a backslash and n.")
+# print('Day 1 - Python Print Function')
+# print('The function is declared like this:')
+# print("print('what to print')")
 
 
-#Question 3
-#Write your code below this line 👇
-print(len( input('What is your name?')))
+# # Question 2
+
+# #Fix the code below 
+
+# print("Day 1 - String Manipulation")
+# print("String Concatenation is done with the "'"+"'" sign.")
+# print('e.g. print("Hello " + "world")')
+# print("New lines can be created with a backslash and n.")
+
+
+# #Question 3
+# #Write your code below this line 
+# print(len( input('What is your name?')))
 
 # Question 4 
-# 🚨 Don't change the code below 👇
-a = input("a: ")
-b = input("b: ")
-# 🚨 Don't change the code above 👆
+#Don't change the code below 
+#a = input("a: ")
+#b = input("b: ")
+#Don't change the code above 
 
-####################################
-#Write your code below this line 👇
-cup1 = 0
+# ####################################
+# #Write your code below this line 
+# cup1 = 0
 
-cup1 = a
-a = b 
-b = cup1
-#Write your code above this line 👆
-####################################
+# cup1 = a
+# a = b 
+# b = cup1
+# #Write your code above this line 
+# ####################################
 
-# 🚨 Don't change the code below 👇
-print("a: " + a)
-print("b: " + b)
+# #  Don't change the code below
+# print("a: " + a)
+# print("b: " + b)
 
+
+#1. Create a greeting for your program.
+# print('Hi welcome to my code')
+
+# #2. Ask the user for the city that they grew up in.
+# city = input('What city did you grow up in?:' )
+
+
+# #3. Ask the user for the name of a pet.
+# pet = input('What is the name of your pet?: ')
+
+
+# #4. Combine the name of their city and pet and show them their band name.
+# print(city + pet )
+
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://replit.com/@appbrewery/band-name-generator-end
+
+
+######### BMI Calculator#######################
+#  Don't change the code below 
+# height = input("enter your height in m: ")
+# weight = input("enter your weight in kg: ")
+# # Don't change the code above 
+
+# #Write your code below this line 
+
+# new_height = float(height)
+# new_weight = float(weight)
+
+# bmi = new_weight/(new_height ** 2)
+
+
+
+# print(int(bmi))
+################################################
 
---
+++ day2.py
@@ -0,0 +1,19 @@
+# 🚨 Don't change the code below 
+# two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 
+
+####################################
+#Write your code below this line 
+# print(type(two_digit_number))
+
+# first = two_digit_number[0]
+# second = two_digit_number[1]
+
+# print(two_digit_number[0])
+# print(two_digit_number[1])
+
+# results = int(two_digit_number[0]) + int(two_digit_number[1])
+
+# print(results)
+#################################################################
+
